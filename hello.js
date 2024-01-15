920f6ef81fae8f629576ff852294a3ae819c970c
Sharmin Sultana <sharmin9518@gmail.com>
2024-01-15 11:36:12 +0600

    js-practice

---
+++ hello.js
@@ -0,0 +1,257 @@
+// document.getElementById("demo").innerHTML = "I am from KAPASIA";
+// document.getElementById("demo").style.color = "Pink";
+/*function myFunction(){
+    document.getElementById('demo').innerHTML = "Sharmin Sultama";
+}*/
+// console.log(5+6);
+/*let a,b,c;
+    a = 5;
+    b = 6;
+    c = a+b;
+    console.log(c);*/
+    // console.log("sharmin"+" "+ "Sultana" );
+
+// var carName = " Volvo";
+// console.log(typeof carName);
+// var carNme = "BMW";
+// console.log(carName);
+// let carName = "Volvo";
+//     // console.log(carName);
+//     carName = "BMW";
+//     console.log(carName);
+       
+ // Create an Array:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// // Change an element:
+// cars[0] = "Toyota";
+
+// // Add an element:
+// cars.push("Audi");
+// const cousin = [ 'sharmin', 'sonali','Fahmida'];
+// console.log(cousin);
+
+// cousin[0] = 'sumaiya';
+// console.log(cousin);
+// const car = {
+//     type: "BMw",
+//     model:"500",
+//     color: "red"
+// };
+// console.log(car);
+// const student = {
+//     name:"Sharmin Sultana",
+
+// }
+// const student = {
+//     name   :"Sharmin Sultana",
+//     id     : " 1838942456",
+//     address: "Kapasia"
+
+// };
+// console.log(student);
+
+// const student = {
+//     name : " Sharmin Sultana",
+//     id   : " 1834902565",
+//     address : "Kapsia"
+// }
+
+// let lenghth = 15;
+// let weight = 7.5;
+// let  color = "Yellow";
+// let LastName = " Sultana";
+
+// let x = true;
+// let y = false;
+
+// const person = {
+//     firstName : " Sharmin",
+//     lastName : "Sultana"
+// // }
+// const person1 = {
+//     firstName : " Sharmin sultana",
+//     lastName : "Mitu",
+//     age  : "50",
+//     eyeColor : "blue"
+
+
+// }
+// console.log(typeof person1)
+
+// let car;
+// console.log(typeof car);
+// let car = " ";
+// console.log(typeof car);
+// function myFunction(p1,p2){
+//     return p1 * p2;
+// }
+//  let result = myFunction(1,3);
+//  console.log(result);
+//  function sleep(){
+//     console.log(" Jasim is sleeping from 10 PM");
+//  }
+//     sleep();
+//  function sleep(names){
+//     console.log(names + " is sleeping from 10 PM");
+//  }
+//  sleep("Sharmin");
+//  sleep("Karima");
+//  sleep("Sumaiya");
+   
+// function sleep(name, time){
+//     console.log(name + " is sleeping from "+ time);
+//     return 10;
+//     console.log(12);
+    
+// }
+//     sleep("jasim","10 PM");
+//     console.log (5);
+//  const car = {
+//     type : "Fiat",
+//     model : "500",
+//     color : "White"
+//  }
+// //  console.log(car.model);
+//     console.log(car[model]);
+//  console.log("car");
+//  let text = "ABCDEFGHIJKLMNOPQRST";
+//  console.log( text.length);
+//  let text = " We are \"Happy\" from the inside";
+//  conasole.log(text);
+// let text = "We are the so-called \"Vikings\" from the north.";
+// console.log(text);
+//  let text = "Apple, Banana, kiwi";
+//  let part = text.slice(7,13);
+//  console.log(part);
+
+// let text = " My name is Sharmin Sultana";
+// let index = text.indexOf("is");
+// console.log("index");
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
+// console.log("index");
+// let x = 123e5;
+// let y = 123e-5;
+// console.log(x,y);
+
+// let x = 123e5;
+// let y = 234e-5;
+// // console.log(x,y);
+// let = 0.3 + 0.5;
+// console.log(let);
+// let x = ( 0.2*10 + 0.1*10)/10;
+// console.log(x);
+// let x = 10;
+// let y = '20';
+// let z = x + y;
+// console.log(z);
+// let x = "10";
+// let y = 20;
+// let z = x + y;
+
+// console.log(z);
+
+// let x = 10;
+// let y = 20;
+// let z = "The result is: " +x +y;
+
+// console.log(z);
+
+// let x = 10;
+// let y = 20;
+// let z = "30";
+
+// let result = x + y + z;
+
+// console.log(result);
+
+// let x = "100";
+// let y = "10";
+// let z = x/y;
+// console.log(z);
+
+// let x = 100 / "Apple";
+
+// console.log(x);
+
+// let x = NaN;
+// let y = 5;
+// let z =  x + y;
+// console.log(z);
+// let x = NaN;
+// let y = "5";
+// let z = x + y;
+
+// console.log(z);
+// // console.log(typeof(z));
+//  const cars = [ " saab", "Volvo","BMW"];
+//  console.log(cars[0]);
+
+// const cousin = [ "Sharmin", "Sumaiya", "Rahima", " karima"];
+// updatedCousin = cousin.toString();
+// console.log(typeof(Cousin));
+// console.log(cousin);
+// console.log(typeof(cousin));
+// let length = cousin.length;
+// console.log(length.length - 1);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits[fruits.length - 1];
+// console.log(fruit);
+
+// const fruits = ["Banana", "Orange","Apple", "Apple"];
+// let fruit = fruits[fruits.length - 1];
+// // console.log(fruit);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fLen = fruits.length;
+
+// let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>";
+// console.log(text);
+//  var sunglassPrice = 120;
+//  var specialName = "Sharmin";
+//  var friendName = ["sharmin","sultana","Mitu","Fahmida"];
+//  console.log(friendName.length);
+
+// var cousinName = [ 'sharmin','sunaiya','nasima','josim','rasel'];
+// console.log(cousinName[2]);
+// cousinName[1] = 'sumu';
+// console.log(cousinName);
+
+// var number = [ 10,12,13,14,15];
+// console.log(number);
+// number[1] = 50;
+// console.log(number);
+// console.log(number.length);
+// console.log
+//  var cousin = ["Sharmin","Sumaiya","Nasima","karima"];
+//  console.log(cousin);
+// console.log(cousin.indexOf(Sha));
+ ss
+// var number = [ 10,11,12 ,13 ,13,14];
+// console.log(number.indexof(10));
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.indexOf("Apple"));
+console.log(fruits.length);
+console.log(fruits[0]);
+fruits[0]="sharmin";
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
